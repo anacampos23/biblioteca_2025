efcac55d48bbf024c7fd2c3264de01e8d33a19d3
anacampos <acamposaguilar23@gmail.com>
2025-03-26 13:18:51 +0100

    Creando modelo y migración de Floor

--- src/Domain/Floor/Models/Floor.php
+++ src/Domain/Floor/Models/Floor.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace App;
+namespace Domain\Floor\Models;
+
+
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Floor extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'zone_numbers',
+    ];
+
+
 }
