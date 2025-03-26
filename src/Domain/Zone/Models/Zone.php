85ccc9056d62ad764f2d01cd2796262e49e96d28
anacampos <acamposaguilar23@gmail.com>
2025-03-26 14:42:38 +0100

    Creando modelo y migración de Floor

--- src/Domain/Zone/Models/Zone.php
+++ src/Domain/Zone/Models/Zone.php
@@ -1,14 +1,12 @@
 <?php
 
-namespace Domain\Floor\Models;
-
-
+namespace Domain\Zone\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
-class Floor extends Model
+class Zone extends Model
 {
     use HasFactory, HasUuids;
 
@@ -19,7 +17,6 @@ class Floor extends Model
      */
     protected $fillable = [
         'name',
+        'zone_numbers',
     ];
-
-
 }
