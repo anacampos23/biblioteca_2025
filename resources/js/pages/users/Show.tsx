f5a53ee51aea162f47a51f1d39b43530de10b4dd
anacampos23 <acamposaguilar23@gmail.com>
2025-05-05 15:09:11 +0200

    Timeline para cada usuario

--- resources/js/pages/users/Show.tsx
+++ resources/js/pages/users/Show.tsx
@@ -3,7 +3,7 @@ import { Separator } from '@/components/ui/separator';
 import { useTranslations } from '@/hooks/use-translations';
 import { BookLayout } from '@/layouts/books/BookLayout';
 import { PageProps } from '@inertiajs/core';
-import { Book } from 'lucide-react';
+import { MessageSquareWarning  } from 'lucide-react';
 import { Link } from "@inertiajs/react";
 import { UserLayout } from '@/layouts/users/UserLayout';
 import UserTimeline from './components/UserTimeline';
@@ -49,14 +49,25 @@ interface ShowUserProps {
     combined: CombinedItem[];
 }
 
-
 export default function Show({ user, loans, reserves, combined }: ShowUserProps) {
     const { t } = useTranslations();
+    // Verificar si existen loans o reserves
+    const hasLoansOrReserves = loans.some((loan) => loan.user_id === user.id) || reserves.some((reserve) => reserve.user_id === user.id);
     return (
         <UserLayout title={t('ui.users.timeline')}>
-            <div className="flex flex-col items-center w-full min-h-screen px-6 py-8 bg-gray-50 dark:bg-gray-800">
-                <div className="flex flex-col w-full max-w-4xl mx-auto">
-                    <UserTimeline user={user} loans={loans} reserves={reserves} combined={combined} />
+            <div className="flex min-h-screen w-full flex-col items-center px-6 py-8">
+                <div className="mx-auto flex w-full max-w-4xl flex-col">
+                    {/* Si hay loans o reserves, muestra el UserTimeline, si no, muestra el mensaje */}
+                    {hasLoansOrReserves ? (
+                        <UserTimeline user={user} loans={loans} reserves={reserves} combined={combined} />
+                    ) : (
+                        <div className="mt-20 flex flex-col items-center justify-center rounded-lg p-6 text-center text-xl font-semibold text-gray-700 dark:text-gray-100">
+                            <div className="mb-4 text-9xl text-gray-700 dark:text-gray-100">
+                                <MessageSquareWarning />
+                            </div>
+                            <p className="text-lg">{t('ui.users.no_loans_reserves')}</p>
+                        </div>
+                    )}
                 </div>
             </div>
         </UserLayout>
