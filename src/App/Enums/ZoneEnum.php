9bfa9a7ffc85b3b16e182ae666a95170806f845c
anacampos23 <acamposaguilar23@gmail.com>
2025-05-06 12:14:38 +0200

    Estadísticas

---
+++ src/App/Enums/ZoneEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+enum ZoneEnum: string
+{
+    case Zone1 = 'Literature';
+    case Zone2 = 'Novel';
+    case Zone3 = 'Science and Technology';
+    case Zone4 = 'Humanities';
+    case Zone5 = 'Art';
+    case Zone6 = 'Lifestyle';
+    case Zone7 = 'Children';
+    case Zone8 = 'Young Adult';
+
+}
--- src/App/Statistics/Controllers/StatisticController.php
+++ src/App/Statistics/Controllers/StatisticController.php
@@ -19,16 +19,39 @@ use Inertia\Response;
 use Domain\Users\Models\User;
 use Domain\Books\Models\Book;
 use Domain\Reserves\Models\Reserve;
+use Domain\Zones\Models\Zone;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Enums\ZoneEnum;
 
 use function Laravel\Prompts\alert;
 
 class StatisticController extends Controller
 {
     public function index()
-{
-        $books = Book::select(['id', 'title', 'author', 'ISBN'])->withTrashed()->get()->toArray();
-        $users = User::select(['id', 'name', 'email'])->withTrashed()->orderBy('name', 'asc')->get()->toArray();
+    {
+        $books = DB::table('books')
+            ->leftJoin('loans', 'books.id', '=', 'loans.book_id')
+            ->select('books.ISBN', 'books.title', DB::raw('COUNT(loans.id) as loans_count'))
+            ->groupBy('books.ISBN', 'books.title')
+            ->orderByDesc('loans_count')
+            ->get();
+
+        $users = DB::table('users')
+            ->leftJoin('loans', 'users.id', '=', 'loans.user_id')
+            ->leftJoin('reserves', 'users.id', '=', 'reserves.user_id')
+            ->select(
+                'users.email',
+                'users.name',
+                DB::raw('COUNT(DISTINCT loans.id) as loans_count'),
+                DB::raw('COUNT(DISTINCT reserves.id) as reserves_count')
+            )
+            ->groupBy('users.email', 'users.name')
+            ->havingRaw('COUNT(DISTINCT loans.id) > 0 OR COUNT(DISTINCT reserves.id) > 0')
+            ->orderByDesc('loans_count')
+            ->get();
+
+
         $loans = Loan::withTrashed()
             ->with(['book:id,title,author,ISBN'])
             ->select(['id', 'start_loan', 'end_loan', 'due_date', 'active', 'user_id', 'book_id'])
@@ -36,11 +59,26 @@ class StatisticController extends Controller
             ->get()
             ->toArray();
 
-    // Pasamos los datos a la vista Inertia
-    return Inertia::render('statistics/Index', [
-        'loans' => $loans,
-        'users' => $users,
-        'books' => $books,
-    ]);
-}
+        $zones_movement = Zone::withTrashed()
+            ->leftJoin('books', 'zones.id', '=', 'books.zone_id')
+            ->leftJoin('loans', 'books.id', '=', 'loans.book_id')
+            ->leftJoin('reserves', 'books.id', '=', 'reserves.book_id')
+            ->select(
+                'books.zone_id',
+                'zones.name as zone_name',
+                DB::raw('COUNT(loans.id) as loans_count'),
+                DB::raw('COUNT(DISTINCT reserves.id) as reserves_count')
+            )
+            ->groupBy('books.zone_id', 'zones.name')
+            ->orderByDesc('loans_count')
+            ->get();
+
+
+        return Inertia::render('statistics/Index', [
+            'loans' => $loans,
+            'users' => $users,
+            'books' => $books,
+            'zones_movement' => $zones_movement,
+        ]);
+    }
 }
